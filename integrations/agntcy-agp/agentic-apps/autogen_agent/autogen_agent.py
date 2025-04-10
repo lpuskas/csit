d4decf56b99cedfad9c44fa44eacb39f9967ed7f
Peter Balogh <p.balogh.sa@gmail.com>
2025-04-10 09:15:56 +0200

    fix: update gw test agetic apps (#59)
    * fix: update AGP python bindings
    
    Signed-off-by: Peter Balogh <p.balogh.sa@gmail.com>
    
    * fix: agent IDs
    
    Signed-off-by: Peter Balogh <p.balogh.sa@gmail.com>
    
    * fix: missing default iteration
    
    Signed-off-by: Peter Balogh <p.balogh.sa@gmail.com>
    
    * fix: revert agetnic app version
    
    Signed-off-by: Peter Balogh <p.balogh.sa@gmail.com>
    
    * fix: update binding and fix langchain agent
    
    Signed-off-by: Peter Balogh <p.balogh.sa@gmail.com>
    
    * fix: update agp-bindings to 0.2.3
    
    Signed-off-by: Peter Balogh <p.balogh.sa@gmail.com>
    
    ---------
    
    Signed-off-by: Peter Balogh <p.balogh.sa@gmail.com>

--- integrations/agntcy-agp/agentic-apps/autogen_agent/autogen_agent.py
+++ integrations/agntcy-agp/agentic-apps/autogen_agent/autogen_agent.py
@@ -6,47 +6,107 @@ from simple_agentic_app.simple_agentic_app import simple_autogen_app
 
 import argparse
 import agp_bindings
+from agp_bindings import GatewayConfig
 
-gateway = agp_bindings.Gateway()
 
-async def run_agent(address):
+async def run_agent(message, address, iterations):
     agent = simple_autogen_app()
 
     local_organization = "cisco"
     local_namespace = "default"
     local_agent = "autogen"
 
-    # Connect to the gateway server
-    local_agent_id = await gateway.create_agent(
-        local_organization, local_namespace, local_agent
-    )
+    remote_organization = "cisco"
+    remote_namespace = "default"
+    remote_agent = "langchain"
 
-    # Connect to the service and subscribe for the local name
-    _ = await gateway.connect(address, insecure=True)
-    await gateway.subscribe(
-        local_organization, local_namespace, local_agent, local_agent_id
-    )
+    # create new gateway object
+    gateway = await agp_bindings.Gateway.new(local_organization, local_namespace, local_agent)
 
-    while True:
-        # receive messages
-        src, msg = await gateway.receive()
+    # Configure gateway
+    config = GatewayConfig(endpoint=address, insecure=True)
+    gateway.configure(config)
 
-        # handle received messages
-        result = await agent.initate_chat(msg)
-        print(result)
+    # Connect to remote gateway server
+    print(f"connecting to: {address}")
+    _ = await gateway.connect()
 
-        # process response
-        result.inner_messages
-        weather_question = result.inner_messages[-1].content[-1].content.split(":")
-        if weather_question[0] == "WEATHER":
-            print("about to send back: ", weather_question[1])
-            await gateway.publish_to(weather_question[1].encode(), src)
+    # Get the local agent instance from env
+    instance = "autogen_instance"
 
-def main():
+    async with gateway:
+        if message:
+            # Create a route to the remote ID
+            await gateway.set_route(remote_organization, remote_namespace, remote_agent)
+
+            # create a session
+            session = await gateway.create_ff_session(agp_bindings.PyFireAndForgetConfiguration())
+
+            for i in range(0, iterations):
+                try:
+                    # Send the message
+                    await gateway.publish(
+                        session,
+                        message.encode(),
+                        remote_organization,
+                        remote_namespace,
+                        remote_agent,
+                    )
+                    print(f"{instance} sent:", message)
+
+                    # Wait for a reply
+                    session_info, msg = await gateway.receive(session=session.id)
+                    print(
+                        f"{instance.capitalize()} received (from session {session_info.id}):",
+                        f"{msg.decode()}",
+                    )
+                except Exception as e:
+                    print("received error: ", e)
+
+                await asyncio.sleep(1)
+        else:
+            # Wait for a message and reply in a loop
+            while True:
+                session_info, _ = await gateway.receive()
+                print(
+                    f"{instance.capitalize()} received a new session:",
+                    f"{session_info.id}",
+                )
+
+                async def background_task(session_id):
+                    while True:
+                        # Receive the message from the session
+                        session, msg = await gateway.receive(session=session_id)
+                        print(
+                            f"{instance.capitalize()} received (from session {session_id}):",
+                            f"{msg.decode()}",
+                        )
+
+                        # handle received messages
+                        result = await agent.initate_chat(msg)
+                        print(result)
+
+                        # process response
+                        result.inner_messages
+                        weather_question = result.inner_messages[-1].content[-1].content.split(":")
+                        if weather_question[0] == "WEATHER":
+                            await gateway.publish_to(session, weather_question[1].encode())
+                            print(f"{instance.capitalize()} replies:", weather_question[1])
+
+                asyncio.create_task(background_task(session_info.id))
+
+
+async def main():
     parser = argparse.ArgumentParser(description="Command line client for message passing.")
     parser.add_argument("-g", "--gateway", type=str, help="Gateway address.", default="http://127.0.0.1:12345")
+    parser.add_argument("-m", "--message", type=str, help="Message to send.")
+    parser.add_argument("-i", "--iterations",type=int,help="Number of messages to send, one per second.", default=1)
     args = parser.parse_args()
-    asyncio.run(run_agent(args.gateway))
+    await run_agent(args.message, args.gateway, args.iterations)
+
 
 if __name__ == "__main__":
-    main()
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Program terminated by user.")
--- integrations/agntcy-agp/agentic-apps/langchain_agent/langchain_agent.py
+++ integrations/agntcy-agp/agentic-apps/langchain_agent/langchain_agent.py
@@ -9,54 +9,99 @@ from simple_weather_agent.simple_weather_agent import (
 
 import argparse
 import agp_bindings
+from agp_bindings import GatewayConfig
 
-gateway = agp_bindings.Gateway()
 
-async def run_agent(message, address):
-
+async def run_agent(message, address, iterations):
     agent = SIMPLE_WEATHER_AGENT_WITH_TOOLS()
 
     local_organization = "cisco"
     local_namespace = "default"
     local_agent = "langchain"
 
-    # Connect to the gateway server
-    local_agent_id = await gateway.create_agent(
-        local_organization, local_namespace, local_agent
-    )
-
-    # Connect to the service and subscribe for the local name
-    _ = await gateway.connect(address, insecure=True)
-    await gateway.subscribe(
-        local_organization, local_namespace, local_agent, local_agent_id
-    )
-
     remote_organization = "cisco"
     remote_namespace = "default"
     remote_agent = "autogen"
 
-    await gateway.set_route(remote_organization, remote_namespace, remote_agent)
+    # create new gateway object
+    gateway = await agp_bindings.Gateway.new(local_organization, local_namespace, local_agent)
 
-    await gateway.publish(message.encode(), remote_organization, remote_namespace, remote_agent)
-    print(f"sent: {str(message)}")
+    # Configure gateway
+    config = GatewayConfig(endpoint=address, insecure=True)
+    gateway.configure(config)
 
-    try:
-        # Wait to receive a message
-        source, msg_rcv = await gateway.receive()
-        msg = msg_rcv.decode()
-        print(f"received: {str(msg)}")
-    except asyncio.CancelledError:
-        print(f"stopped.")
+    # Connect to remote gateway server
+    print(f"connecting to: {address}")
+    _ = await gateway.connect()
 
-    result = agent.call(msg)
-    print(result)
+    # Get the local agent instance from env
+    instance = "langchain_instance"
 
-def main():
+    async with gateway:
+        if message:
+            # Create a route to the remote ID
+            await gateway.set_route(remote_organization, remote_namespace, remote_agent)
+
+            # create a session
+            session = await gateway.create_ff_session(agp_bindings.PyFireAndForgetConfiguration())
+
+            for i in range(0, iterations):
+                try:
+                    # Send the message
+                    await gateway.publish(
+                        session,
+                        message.encode(),
+                        remote_organization,
+                        remote_namespace,
+                        remote_agent,
+                    )
+                    print(f"{instance} sent:", message)
+
+                    # Wait for a reply
+                    session_info, msg = await gateway.receive(session=session.id)
+                    print(
+                        f"{instance.capitalize()} received (from session {session_info.id}):",
+                        f"{msg.decode()}",
+                    )
+                except Exception as e:
+                    print("received error: ", e)
+
+                # handle received messages
+                result = agent.call(msg)
+                print(result)
+
+                await asyncio.sleep(1)
+        else:
+            # Wait for a message and reply in a loop
+            while True:
+                session_info, _ = await gateway.receive()
+                print(
+                    f"{instance.capitalize()} received a new session:",
+                    f"{session_info.id}",
+                )
+
+                async def background_task(session_id):
+                    while True:
+                        # Receive the message from the session
+                        session, msg = await gateway.receive(session=session_id)
+                        print(
+                            f"{instance.capitalize()} received (from session {session_id}):",
+                            f"{msg.decode()}",
+                        )
+
+                asyncio.create_task(background_task(session_info.id))
+
+
+async def main():
     parser = argparse.ArgumentParser(description="Command line client for message passing.")
     parser.add_argument("-m", "--message", type=str, help="Message to send.")
     parser.add_argument("-g", "--gateway", type=str, help="Gateway address.", default="http://127.0.0.1:12345")
+    parser.add_argument("-i", "--iterations",type=int,help="Number of messages to send, one per second.", default=1)
     args = parser.parse_args()
-    asyncio.run(run_agent(args.message, args.gateway))
+    await run_agent(args.message, args.gateway, args.iterations)
 
 if __name__ == "__main__":
-    main()
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Program terminated by user.")
